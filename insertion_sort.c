07dbdd2daae4dcab77b5f652cfb0b0621dcd671a
Ali Khan <alkhan@f0r2s17.codam.nl>
2026-05-02 15:55:06 +0200

    fixed set_stack_b funtion and started on find_pos_in_b

--- insertion_sort.c
+++ insertion_sort.c
@@ -6,10 +6,12 @@
 /*   By: alkhan <alkhan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 17:49:26 by alkhan            #+#    #+#             */
-/*   Updated: 2026/05/01 17:42:11 by alkhan           ###   ########.fr       */
+/*   Updated: 2026/05/02 15:43:53 by alkhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include "list_utils.h"
 #include "operations.h"
 #include "sorting.h"
 #include "stack.h"
@@ -34,41 +36,64 @@ bool	is_stack_sorted_rev(t_stack *stack)
 static void	insertion_sort(t_stacks *stacks)
 {
 	if (set_stack_b(stacks) != 1)
-		return ;		
-	move_and_sort_to(stacks);
+		return ;
+	move_and_sort_to(stacks, B);
 	rev_move_to(stacks);
 }
+int	find_pos_in_b(t_stacks *stacks, int value)
+{
+	int		max_value;
+	int		min_value;
+	t_list	*node_b;
 
-void move_and_sort_to(t_stacks *stacks, t_stack_name *stackname)
-{ 
-	int listsize;
+	node_b = stacks->b.top;
+	max_value = find_max_value();
+	min_value = find_min_value();
+	if (value > max_value)
+	{
+		if (find_max_pos() == 0)
+			return (0);
+		else
+			return (find_max_pos() - 1);
+	}
+	while (node_b)
+	{
+		if (value > get_content(node_b)->value)
+			;
+		node_b = node_b->next;
+	}
+}
+void	move_and_sort_to(t_stacks *stacks, t_stack_name *stackname)
+{
+	int	listsize;
 
-	lis
 	listsize = ft_lstsize(stacks->a.top);
-	while( listsize == 0)
+	while (listsize > 0)
 		stacks->b.top->content
-}	
-// This function is going to set up stack B, before insertion starts, add 3 numbers from stack a, and sort them.
+}
+// This function is going to set up stack B, before insertion starts,
+// add 3 numbers from stack a, and sort them.
+
 int	set_stack_b(t_stacks *stacks)
 {
-	while(ft_lstsize(stacks->b.top) < 3)
-		push_b(stacks->a);  
+	while (ft_lstsize(stacks->b.top) < 3)
+		opp_push(stacks, A);
 	if (is_stack_sorted_rev(stacks->b.top) == true)
 		return (1);
-	if (stacks->b.top->content > stacks->b.top->next->content && 
-		stacks->b.top->content > stacks->b.top->next->next->content)
-			return(rotate(stacks->b), swap(stacks->b), 1);
-	else if (stacks->b.top->content > stacks->b.top->next->content && 
-		stacks->b.top->content < stacks->b.top->next->next->content)
-		return(reverse_rotate(stacks->b), 1);
-	else if (stacks->b.top->content < stacks->b.top->next->content && 
-		stacks->b.top->next->next->content < stacks->b.top->next->content)
-		return(rotate(stacks->b), 1);
-	else if (stacks->b.top->next->next->content > stacks->b.top->next->content && 
-		stacks->b.top->next->content > stacks->b.top->content)
-		return(swap(stacks->b), reverse_rotate(stacks->b) , 1);
-	else if (stacks->b.top->next->content > stacks->b.top->content && 
-		stacks->b.top->next->content > stacks->b.top->next->next->content)
-		return(swap(stacks->b), 1);
+	if (get_content(stacks->b.top)->value > get_content(stacks->b.top->next)->value
+		&& get_content(stacks->b.top)->value > get_content(stacks->b.top->next->next)->value)
+		return (rotate(stacks->b), swap(stacks->b), 1);
+	else if (get_content(stacks->b.top)->value > get_content(stacks->b.top->next)->value
+		&& get_content(stacks->b.top)->value < get_content(stacks->b.top->next->next)->value)
+		return (reverse_rotate(stacks->b), 1);
+	else if (get_content(stacks->b.top)->value < get_content(stacks->b.top->next)->value
+		&& get_content(stacks->b.top->next->next)->value < get_content(stacks->b.top->next)->value)
+		return (rotate(stacks->b), 1);
+	else if (get_content(stacks->b.top->next->next)->value > get_content(stacks->b.top->next)->value
+		&& get_content(stacks->b.top->next)->value > get_content(stacks->b.top)->value)
+		return (swap(stacks->b), reverse_rotate(stacks->b), 1);
+	else if (get_content(stacks->b.top->next)->value > get_content(stacks->b.top)->value
+		&& get_content(stacks->b.top->next)->value > get_content(stacks->b.top->next->next)->value)
+		return (swap(stacks->b), 1);
 	return (-1);
 }
